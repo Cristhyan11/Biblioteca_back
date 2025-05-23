88179c49e311e29b10501882e4fdd4101f9577a7
cristhyan vera <cristhian.ver01@gmail.com>
2025-05-22 21:45:42 -0500

    Bugfix 0.2

--- dist/app.js
+++ dist/app.js
@@ -10,9 +10,14 @@ const writer_routes_1 = __importDefault(require("./routes/writer.routes"));
 const book_routes_1 = __importDefault(require("./routes/book.routes"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "http://localhost:5173" }));
+app.use((0, cors_1.default)({
+    origin: [
+        "http://localhost:5173",
+        "https://biblioteca-front.vercel.app" // Cambia esto por la URL real de tu frontend en Vercel
+    ],
+    credentials: true,
+}));
 app.use(express_1.default.json());
-app.get("/health", (_req, res) => res.json({ ok: true }));
 app.use("/writers", writer_routes_1.default);
 app.use("/books", book_routes_1.default);
 app.use((req, res) => res.status(404).json({ message: "Not Found" }));
@@ -20,17 +25,4 @@ app.use((err, _req, res, _next) => {
     console.error(err);
     res.status(500).json({ message: err.message });
 });
-/*
-const PORT = process.env.PORT || 3000;
-
-AppDataSource.initialize()
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`🚀 Server running on http://localhost:${PORT}`)
-    )
-  )
-  .catch(err => {
-    console.error("DB init error:", err);
-    process.exit(1);
-  });*/
 exports.default = app;
